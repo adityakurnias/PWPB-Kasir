c5851e18360cb6452f203db2e9e6599bd834edb8
adityakurnias <adityakurniasaputra903@gmail.com>
2025-11-12 15:40:14 +0700

    Half of pertemuan 3

--- resources/views/livewire/user.blade.php
+++ resources/views/livewire/user.blade.php
@@ -1,3 +1,21 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <div class="container">
+        <div class="row my-2">
+            <div class="col-12">
+                <button wire:click="pilihMenu('lihat')"
+                    class="btn {{ $pilihanMenu == 'lihat' ? 'btn-primary' : 'btn-outline-primary' }}">
+                    Semua Pengguna
+                </button>
+    
+                <button wire:click="pilihMenu('tambah')"
+                    class="btn {{ $pilihanMenu == 'tambah' ? 'btn-primary' : 'btn-outline-primary' }}">
+                    Tambah Pengguna
+                </button>
+    
+                <button wire:loading class="btn btn-info">
+                    Loading ...
+                </button>
+            </div>
+        </div>
+    </div>
 </div>
